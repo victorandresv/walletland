dffa4188b033dfc0f2b2eb72888bded1ec011450
Victor Vargas <Victor Vargas>
2017-05-23 17:46:20 -0400

    balance screen OK

---
+++ app/src/main/java/walletland/movitech/cl/walletland/BalanceActivity.java
@@ -0,0 +1,13 @@
+package walletland.movitech.cl.walletland;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BalanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance);
+    }
+}
