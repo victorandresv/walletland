0dcb0982a9d4ffe7554be3c6531387f7c6de5df7
Victor Vargas <Victor Vargas>
2017-05-25 10:36:10 -0400

    avance walletland

--- app/src/main/java/walletland/movitech/cl/walletland/BalanceActivity.java
+++ app/src/main/java/walletland/movitech/cl/walletland/BalanceActivity.java
@@ -1,13 +1,53 @@
 package walletland.movitech.cl.walletland;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BalanceActivity extends AppCompatActivity {
+
+    TextView textBalance;
+    Button buttonIn;
+    Button buttonOut;
+    Intent intentBalancerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balance);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        textBalance = (TextView) findViewById(R.id.textBalance);
+        buttonIn = (Button) findViewById(R.id.buttonIn);
+        buttonOut = (Button) findViewById(R.id.buttonOut);
+        intentBalancerList = new Intent(this, BalancerListActivity.class);
+
+        buttonIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentBalancerList.putExtra("option", "in");
+                startActivityForResult(intentBalancerList, 1);
+            }
+        });
+        buttonOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentBalancerList.putExtra("option", "out");
+                startActivityForResult(intentBalancerList, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+
+        }
     }
 }
---
+++ app/src/main/java/walletland/movitech/cl/walletland/BalancerListActivity.java
@@ -0,0 +1,58 @@
+package walletland.movitech.cl.walletland;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class BalancerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balancer_list);
+        getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.balancer_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addNew:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.animation_enter, R.anim.animation_leave);
+    }
+}
